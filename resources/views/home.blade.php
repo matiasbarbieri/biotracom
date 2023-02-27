af947dccd2c67077c376c445b858e832d8583ff2
Matias Barbieri <matias.nbarbieri@gmail.com>
2023-02-27 10:23:48 -0300

    gitignore

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,86 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-</head>
-<body>
-    <p>Vista Home</p>
-</body>
-</html>
+<section class="about-one" id="about">
+    <div class="about-one-bg wow slideInRight" data-wow-delay="100ms" data-wow-duration="2500ms" style="background-image: url(&quot;assets/images/backgrounds/about-one-bg.png&quot;); visibility: hidden; animation-duration: 2500ms; animation-delay: 100ms; animation-name: none;"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-6">
+                <div class="about-one__left">
+                    <div class="about-one__img-box wow slideInLeft" data-wow-delay="100ms" data-wow-duration="2500ms" style="visibility: hidden; animation-duration: 2500ms; animation-delay: 100ms; animation-name: none;">
+                        <div class="about-one__img">
+                            <img src="assets/images/resources/about-one-img-1.jpg" alt="">
+                        </div>
+                        <div class="about-one__img-two">
+                            <img src="assets/images/resources/about-one-img-2.jpg" alt="">
+                        </div>
+                        <div class="about-one__experience">
+                            <h2 class="about-one__experience-year">30</h2>
+                            <p class="about-one__experience-text">Years of <br> Experience</p>
+                        </div>
+                        <div class="about-one__shape-1">
+                            <img src="assets/images/shapes/about-one-shape-1.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-6">
+                <div class="about-one__right">
+                    <div class="section-title text-left">
+                        <div class="section-sub-title-box">
+                            <p class="section-sub-title">About company</p>
+                            <div class="section-title-shape-1">
+                                <img src="assets/images/shapes/section-title-shape-1.png" alt="">
+                            </div>
+                            <div class="section-title-shape-2">
+                                <img src="assets/images/shapes/section-title-shape-2.png" alt="">
+                            </div>
+                        </div>
+                        <h2 class="section-title__title">We provide the best insurance policy</h2>
+                    </div>
+                    <p class="about-one__text-1">Duis aute irure dolor in reprehenderit in velit esse cillum
+                        dolore eu nulla pariatur.</p>
+                    <ul class="list-unstyled about-one__points">
+                        <li>
+                            <div class="icon">
+                                <i class="fa fa-check"></i>
+                            </div>
+                            <div class="text">
+                                <p>Refresing to get such a personal touch.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="icon">
+                                <i class="fa fa-check"></i>
+                            </div>
+                            <div class="text">
+                                <p>Duis aute irure dolor in reprehenderit in voluptate.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="icon">
+                                <i class="fa fa-check"></i>
+                            </div>
+                            <div class="text">
+                                <p>Velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                            </div>
+                        </li>
+                    </ul>
+                    <p class="about-one__text-2">Nullam eu nibh vitae est tempor molestie id sed ex. Quisque
+                        dignissim maximus ipsum, sed rutrum metus tincidunt et. Sed eget tincidunt ipsum.</p>
+                    <div class="about-one__btn-call">
+                        <div class="about-one__btn-box">
+                            <a href="about.html" class="thm-btn about-one__btn">Discover More</a>
+                        </div>
+                        <div class="about-one__call">
+                            <div class="about-one__call-icon">
+                                <i class="fas fa-phone"></i>
+                            </div>
+                            <div class="about-one__call-content">
+                                <a href="tel:9200368090">+92 (003) 68-090</a>
+                                <p>Call to Our Experts</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
